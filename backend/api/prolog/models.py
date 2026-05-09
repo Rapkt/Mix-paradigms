3358e1aa1504acec84e3c46c036602f33755f4aa
AhmadxSaied <ahmadsaied404@gmail.com>
2026-05-09 23:05:22 +0300

    feat(prolog): made the models

---
+++ backend/api/prolog/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel,Field
+
+
+class recommended_courses(BaseModel):
+    course_name:str = Field(...,
+    description="""The course name""",strict=True)
+    preference:str  = Field(...,
+    description="""The course preference""",strict=True)
+    year:str = Field(...,
+    description="""The course year""",strict=True)
+    difficulty:str = Field(...,
+    description="""difficulty""",strict=True)
+    id:str = Field(...,
+                   """The augmentation between department & row index""")
